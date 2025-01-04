b8f71309e52c7a8742affcfaabd679abd12a52a9
IridiumLotus <dev@aihamibrahim.com>
2025-01-04 17:04:29 +0500

    main.py

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock()
+    dt = 0
+        
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        pygame.time.Clock.tick(60)
+
+    dt = pygame.Clock.tick() / 1000
 
 
 if __name__ == "__main__":
